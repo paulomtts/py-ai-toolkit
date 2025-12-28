a2af830c51548dbed9c590dc86e43fef2d7c1782
paulomtts <paulomtts@outlook.com>
2025-12-28 12:34:27 -0300

    fix: exposed LLMConfig import

--- py_ai_toolkit/__init__.py
+++ py_ai_toolkit/__init__.py
@@ -4,7 +4,7 @@ from grafo import Chunk, Node, TreeExecutor
 
 from .core.base import BaseWorkflow
 from .core.domain.errors import BaseError
-from .core.domain.interfaces import CompletionResponse
+from .core.domain.interfaces import CompletionResponse, LLMConfig
 from .core.domain.models import BaseValidation, ValidationTest
 from .core.tools import PyAIToolkit
 
@@ -18,4 +18,5 @@ __all__ = [
     "BaseError",
     "BaseValidation",
     "ValidationTest",
+    "LLMConfig",
 ]
